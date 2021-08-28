e0dcbaf4856031377e5904bc70f372a917ab5997
ljs38 <dasl5880@gmail.com>
2021-08-28 18:46:51 +0900

    수정

--- Cinema/src/LJS/SignUp.java
+++ Cinema/src/LJS/SignUp.java
@@ -5,10 +5,6 @@ import java.util.List;
 
 public class SignUp {
 
-	private static String id2 = null;
-	private static String password2 = null;
-	private static String phone2 = null;
-
 	public static boolean signUp() {	
 
 		
@@ -17,7 +13,6 @@ public class SignUp {
 		memberList.add(new Member("kimyc","yc456","010-2345-6789"));
 		memberList.add(new Member("leejs","js789","010-3456-7890"));
 		memberList.add(new Member("jeongmj","mj987","010-4567-8901"));
-		memberList.add(new Member(id2, password2, phone2));
 		
 		boolean SignUpSuccess = false;
 		
